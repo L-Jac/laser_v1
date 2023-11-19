55f11cd710528c61961c3f9f8a22575ea47520ab
L-jac <3137501630@qq.com>
2023-11-19 11:01:23 +0800

    lora设置

--- Lora_set/lora_set.py
+++ Lora_set/lora_set.py
@@ -1,8 +1,23 @@
 import serial
 
+print("gun")
 with serial.Serial("/dev/ttyUSB0", 9600, timeout=0.05) as ser:
     buf = [0xc0, 0x00, 0x08,
            0x03, 0xea, 0x84, 0x00, 0x0f, 0x03, 0x00, 0x00]
+    # 0xea枪 0xe9控制箱
+    # uart0枪 uart1控制箱
+    ser.write(bytes(buf))
+    r = ser.read(11)
+    print(r)
+    buf = [0xc1, 0x00, 0x08]
+    ser.write(bytes(buf))
+    r = ser.read(11)
+    print(r)
+
+print("system")
+with serial.Serial("/dev/ttyUSB1", 9600, timeout=0.05) as ser:
+    buf = [0xc0, 0x00, 0x08,
+           0x03, 0xe9, 0x84, 0x00, 0x0f, 0x03, 0x00, 0x00]
     # 0xea枪 0xe9控制箱
     # uart0枪 uart1控制箱
     ser.write(bytes(buf))
